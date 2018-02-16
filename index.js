f2afc0f79904c32697fee8419fadeb708f4f8f99
Dilovan Celik <dilovan.celik@gmail.com>
2018-02-16 14:36:42 +0100

    querying votecount and updating chart, in progress

--- index.js
+++ index.js
@@ -14,17 +14,81 @@ const noWeb3 = function() {
     noWeb3Div.style.display = "block";
 }
 
+
+
 const hasWeb3 = function() {
     const hasWeb3Div = document.getElementById('hasWeb3');
     hasWeb3Div.style.display = "block";
 }
 
+
+
 const vote = function(candidate) {
-    votingContract.vote(candidate, (err, res) => {
+    votingContract.vote(candidate, {
+        gas: 50000
+    }, (err, res) => {
         if(err) {
-            console.log(err);
-        } else {
-            console.log(`You succesfully voted for ${candidate}`);
+            console.log(`Something went wrong, this is the error:\n`);
+            throw err;
         }
+        console.log(res);
     })
+}
+
+const updateCandidateVoteCount = function(_candidate) {
+    votingContract.getCandidateVoteCount(_candidate, (err, res) => {
+        if(err) {
+            console.log(`Something went wrong, this is the error:\n`);
+            throw err;
+        }
+        candidates[_candidate] = res["c"][0];
+    })
+};
+const updateTotalVotecount = async function() {
+    const candidateNames = Object.keys(candidates);
+    candidateNames.forEach((_candidate) => {
+        updateCandidateVoteCount(_candidate);
+    });
+}
+
+const updateChart = async function() {
+    await updateTotalVotecount();
+
+    const ctx = document.getElementById("myChart").getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: Object.keys(candidates) ,
+            datasets: [{
+                label: '# of Votes',
+                data: Object.values(candidates) ,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
 }
